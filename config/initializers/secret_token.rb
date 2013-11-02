98baf6b3f1f8a463a166b004bc3609bfba4fa83a
Zhen Zhang <zbestz@gmail.com>
2013-11-02 14:11:10 +0800

    Weibo App on books inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeiboRails::Application.config.secret_key_base = 'e518bf53dfb261b9210bd6d991a3c10ab73f8694adcfaeb48c694056b2e9cd26b2236cea70df9f4ceffbc6e40490b8d32c66ffd1fbd22ce04a2484c784a51624'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WeiboRails::Application.config.session_store :cookie_store, key: '_weibo_rails_session'
