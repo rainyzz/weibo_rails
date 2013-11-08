530093362006a349e9f6e52038fe05b25c4d7c33
Zhen Zhang <zbestz@gmail.com>
2013-11-08 15:30:56 +0800

    add sign up pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 WeiboRails::Application.routes.draw do
+  resources :users
   match '/signup', to: 'users#new', via: 'get'
   root to: 'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
