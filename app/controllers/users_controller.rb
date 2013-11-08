f6c3f0c4eb36394ff69386c50bbd8f711fbfa035
Zhen Zhang <zbestz@gmail.com>
2013-11-08 19:11:20 +0800

    finish sign in and out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+        sign_in @user
         flash[:success] = "Welcome to the Weibo!"
-     redirect_to @user
+        redirect_to @user
     else
       render 'new'
     end
