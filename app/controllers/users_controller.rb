bd705c4850afaafc37d569b6744c96ab5437d18b
Zhen Zhang <zbestz@gmail.com>
2013-11-14 19:24:39 +0800

    Finish users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,25 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index,:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  def index
+    @users = User.paginate(page: params[:page])
+  end
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      #sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def show
@@ -22,5 +41,15 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+  def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 end
